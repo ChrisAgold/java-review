9a2114675a82db6efa89828508d0e702431ddc24
Chris Agold <christophersagold@gmail.com>
2022-01-04 13:58:01 -0600

    added long example

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -12,12 +12,22 @@ public class Main {
 
         byte myMinByteValue = Byte.MIN_VALUE;
         byte myMaxByteValue = Byte.MAX_VALUE;
-        System.out.println("Byte Maximum Value = " + myMinByteValue);
-        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+//        System.out.println("Byte Maximum Value = " + myMinByteValue);
+//        System.out.println("Byte Maximum Value = " + myMaxByteValue);
 
         short myMinShortValue = Short.MIN_VALUE;
         short myMaxShortValue = Short.MAX_VALUE;
-        System.out.println("Short Maximum Value = " + myMinShortValue);
-        System.out.println("Short Maximum Value = " + myMaxShortValue);
+//        System.out.println("Short Maximum Value = " + myMinShortValue);
+//        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Maximum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+        long bigLongLiteralValue = 2_147_483_647_234L;
+        System.out.println(bigLongLiteralValue);
+
+        short bigShortLiteralValue = 32767;
     }
 }
