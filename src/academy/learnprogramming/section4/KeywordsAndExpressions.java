2b37d382657b0e00afa91dd63c248fd90ed9a086
Chris Agold <christophersagold@gmail.com>
2022-01-15 15:02:13 -0600

    added method and arguments

--- src/academy/learnprogramming/section4/KeywordsAndExpressions.java
+++ src/academy/learnprogramming/section4/KeywordsAndExpressions.java
@@ -1,32 +1,29 @@
 package academy.learnprogramming.section4;
 
 public class KeywordsAndExpressions {
-
     public static void main(String[] args) {
         boolean gameOver = true;
         int score = 5000;
         int levelCompleted = 5;
         int bonus = 100;
 
-//        if(score <  5000 && score > 1000) {
-//            System.out.println("Your score was less than 5000 but greater than 1000.");
-//        } else if(score < 1000) {
-//            System.out.println("Your score was less than 1000");
-//        } else {
-//            System.out.println("Got here.");
-//        }
-
-        if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-//            System.out.println("Your final score was " + finalScore);
-        }
+        // add values for method
+        calculateScore(true, 800, 5, 100);
 
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
-//            System.out.println("Your final score was " + finalScore);
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+
+    // Java Method
+    public static void calculateScore(boolean gameOver,int score, int levelCompleted, int bonus) {
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
         }
     }
 }
