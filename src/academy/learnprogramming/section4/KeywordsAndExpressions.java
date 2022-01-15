6c92e578670a14ac4d35e3b96b5714f02d3abc46
Chris Agold <christophersagold@gmail.com>
2022-01-15 15:43:08 -0600

    added data type int for method

--- src/academy/learnprogramming/section4/KeywordsAndExpressions.java
+++ src/academy/learnprogramming/section4/KeywordsAndExpressions.java
@@ -2,18 +2,20 @@ package academy.learnprogramming.section4;
 
 public class KeywordsAndExpressions {
     public static void main(String[] args) {
-
         // add values for method
         calculateScore(true, 800, 5, 100);
         calculateScore(true, 10000, 8, 200);
     }
 
     // Java Method
-    public static void calculateScore(boolean gameOver,int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
+            return finalScore;
         }
+        // -1 indicates an error
+        return -1;
     }
 }
