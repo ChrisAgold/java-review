17dcf8fa42f80a4e70f9759a6b8b8731db36b51a
Chris Agold <christophersagold@gmail.com>
2022-01-15 16:27:49 -0600

    added displayHighScorePosition & calculateHighScorePosition Methods

--- src/academy/learnprogramming/section4/KeywordsAndExpressions.java
+++ src/academy/learnprogramming/section4/KeywordsAndExpressions.java
@@ -2,9 +2,37 @@ package academy.learnprogramming.section4;
 
 public class KeywordsAndExpressions {
     public static void main(String[] args) {
-        // add values for method
-        calculateScore(true, 800, 5, 100);
-        calculateScore(true, 10000, 8, 200);
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
+    }
+
+    // Methods Challenge 49
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into position " + highScorePosition + " on the high score table!");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if(playerScore > 1000) {
+            return 1;
+        }else if(playerScore > 500 && playerScore < 1000) {
+            return 2;
+        } else if(playerScore > 100 && playerScore < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
     }
 
     // Java Method
@@ -12,10 +40,14 @@ public class KeywordsAndExpressions {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
             return finalScore;
         }
         // -1 indicates an error
         return -1;
     }
+
+
+
+
+
 }
