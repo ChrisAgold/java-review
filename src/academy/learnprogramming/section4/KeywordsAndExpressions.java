4d1c204a34563df89ae02ff2fefdaf158ec1b40b
Chris Agold <christophersagold@gmail.com>
2022-01-15 15:35:01 -0600

    refactored methods

--- src/academy/learnprogramming/section4/KeywordsAndExpressions.java
+++ src/academy/learnprogramming/section4/KeywordsAndExpressions.java
@@ -2,27 +2,17 @@ package academy.learnprogramming.section4;
 
 public class KeywordsAndExpressions {
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 5000;
-        int levelCompleted = 5;
-        int bonus = 100;
 
         // add values for method
         calculateScore(true, 800, 5, 100);
-
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-        if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
+        calculateScore(true, 10000, 8, 200);
     }
 
     // Java Method
     public static void calculateScore(boolean gameOver,int score, int levelCompleted, int bonus) {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
         }
     }
