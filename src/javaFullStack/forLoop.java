5147d8f0e1a33a5b3bfe62b65b38820ae22b832b
Chris Agold <christophersagold@gmail.com>
2022-01-20 16:55:18 -0600

    for loop review

---
+++ src/javaFullStack/forLoop.java
@@ -0,0 +1,10 @@
+package javaFullStack;
+
+public class forLoop {
+    public static void main(String[] args) {
+        for(int i = 1; i<10; i++ ) {
+            System.out.println(i);
+        }
+    }
+
+}
--- src/javaFullStack/switchCase.java
+++ src/javaFullStack/switchCase.java
@@ -1,22 +1,22 @@
 package javaFullStack;
 
-public class switchCase {
-    public static void main(String[] args) {
-        String name = "team";
-        // SWITCH CASE
-        switch (name.toLowerCase()) {
-            case "author":
-                System.out.println("Jimmy");
-                break;
-            case "team":
-                System.out.println("Go Java Team");
-                break;
-            case "editor":
-                System.out.println("Ling & Bob");
-                break;
-            default:
-                System.out.println("Invalid Entry");
-                break;
-        }
-    }
-}
+//public class switchCase {
+//    public static void main(String[] args) {
+//        String name = "team";
+//        // SWITCH CASE
+//        switch (name.toLowerCase()) {
+//            case "author":
+//                System.out.println("Jimmy");
+//                break;
+//            case "team":
+//                System.out.println("Go Java Team");
+//                break;
+//            case "editor":
+//                System.out.println("Ling & Bob");
+//                break;
+//            default:
+//                System.out.println("Invalid Entry");
+//                break;
+//        }
+//    }
+//}
