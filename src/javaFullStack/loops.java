b2f345a031446dde630d60b5100b06972dbd8470
Chris Agold <christophersagold@gmail.com>
2022-01-20 19:09:57 -0600

    added more advance loop examples

--- src/javaFullStack/loops.java
+++ src/javaFullStack/loops.java
@@ -1,7 +1,8 @@
 package javaFullStack;
 
-public class loops {
-    public static void main(String[] args) {
+//public class loops {
+   //public static void main(String[] args) {
+
         // forLoop
 //        for(int i = 1;i <=10; i++) {
 //            System.out.println(i);
@@ -20,13 +21,29 @@ public class loops {
 //            i++;
 //        } while(i<10);
 
-        // i & j for loop
-        for(int i=1, j=1;i<10 || j<10 ; i++,j++) {
-            j++;
-            System.out.println("i="+i+" And j="+j);
-        }
-}
-}
+        // more advance for loop
+//        for(int i=1, j=1;i<10 || j<10 ; i++,j++) {
+//            j++;
+//            System.out.println("i="+i+" And j="+j);
+//        }
+
+        // more advanced while loop
+//        int i = 1, j = 1;
+//        while (i < 10 || j < 10) {
+//            i++;
+//            j++;
+//            System.out.println("i=" + i + " And j=" + j);
+//        }
+
+        // more advance do while loop
+//        int i = 1, j = 1;
+//        do{
+//            i++;
+//            j++;
+//            System.out.println("i=" + i + " And j=" + j);
+//        }while (i < 10 || j < 10);
+//    }
+// }
 
 
 
