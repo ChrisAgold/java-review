9eb3cbd34a657314d0498baf2955a936438e5262
Chris Agold <christophersagold@gmail.com>
2022-01-20 18:22:04 -0600

    doWhile loop example

---
+++ src/javaFullStack/loops.java
@@ -0,0 +1,31 @@
+package javaFullStack;
+
+public class loops {
+    public static void main(String[] args) {
+        // forLoop
+//        for(int i = 1;i <=10; i++) {
+//            System.out.println(i);
+//        }
+
+        // while loop
+//        while(true) {
+//            System.out.println("Infinite loop");
+//            break;
+//        }
+
+        // doWhile
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while(i<10);
+
+    }
+}
+
+
+// Prime number loop
+//    int isPrime = 25;
+//        for (int i = 1; i <= 10; i++) {
+//
+//                }
