0b97ba23981f34bb03df40b9b038e1d225525a37
Chris Agold <christophersagold@gmail.com>
2022-01-21 19:23:22 -0600

    added breaks

--- src/javaFullStack/loops.java
+++ src/javaFullStack/loops.java
@@ -2,31 +2,31 @@ package javaFullStack;
 
 public class loops {
     public static void main(String[] args) {
-
+// *********************************
         // forLoop
 //        for(int i = 1;i <=10; i++) {
 //            System.out.println(i);
 //        }
-
+// *********************************
         // while loop
 //        while(true) {
 //            System.out.println("Infinite loop");
 //            break;
 //        }
-
+// *********************************
         // doWhile
 //        int i = 11;
 //        do {
 //            System.out.println(i);
 //            i++;
 //        } while(i<10);
-
+// *********************************
         // more advance for loop
 //        for(int i=1, j=1;i<10 || j<10 ; i++,j++) {
 //            j++;
 //            System.out.println("i="+i+" And j="+j);
 //        }
-
+// *********************************
         // more advanced while loop
 //        int i = 1, j = 1;
 //        while (i < 10 || j < 10) {
@@ -34,7 +34,7 @@ public class loops {
 //            j++;
 //            System.out.println("i=" + i + " And j=" + j);
 //        }
-
+// *********************************
         // more advance do while loop
 //        int i = 1, j = 1;
 //        do{
@@ -42,7 +42,7 @@ public class loops {
 //            j++;
 //            System.out.println("i=" + i + " And j=" + j);
 //        }while (i < 10 || j < 10);
-
+//*********************************
         // Break & Continue
 //        for (int i = 1; i <= 10; i++) {
 //            System.out.println(i);
@@ -51,7 +51,7 @@ public class loops {
 //            }
 //            System.out.println(i);
 //        }
-
+// *********************************
         // Nested loops
 //        for(int i = 1; i <= 10; i++) {
 //            for(int j=1; j<=10;j++){
@@ -59,20 +59,13 @@ public class loops {
 //            }
 //            System.out.println("*********************************");
 //        }
-
+// *********************************
         // @
-        for(int i = 1; i <= 100; i++) {
-            for(int j=1; j<=i;j++){
-                System.out.print('@');
-            }
-            System.out.println();
-        }
+//        for(int i = 1; i <= 100; i++) {
+//            for(int j=1; j<=i;j++){
+//                System.out.print('@');
+//            }
+//            System.out.println();
+//        }
     }
-
 }
-
-
-
-
-
-
