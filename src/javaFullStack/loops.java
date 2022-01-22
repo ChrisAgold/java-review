da0ba4aaf2d5f06c10dc868b0e44e48036bf2b6b
Chris Agold <christophersagold@gmail.com>
2022-01-21 19:20:07 -0600

    added pattern nested loop

--- src/javaFullStack/loops.java
+++ src/javaFullStack/loops.java
@@ -53,11 +53,19 @@ public class loops {
 //        }
 
         // Nested loops
-        for(int i = 1; i <= 10; i++) {
-            for(int j=1; j<=10;j++){
-                System.out.println("Value of i:"+i+" Value of j:"+j);
+//        for(int i = 1; i <= 10; i++) {
+//            for(int j=1; j<=10;j++){
+//                System.out.println("Value of i:"+i+" Value of j:"+j);
+//            }
+//            System.out.println("*********************************");
+//        }
+
+        // @
+        for(int i = 1; i <= 100; i++) {
+            for(int j=1; j<=i;j++){
+                System.out.print('@');
             }
-            System.out.println("*********************************");
+            System.out.println();
         }
     }
 
