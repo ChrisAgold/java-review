add0ea8aa01616abd3a8966e4d0c8557c62d8c96
Chris Agold <christophersagold@gmail.com>
2022-01-21 19:12:39 -0600

    added nested loop

--- src/javaFullStack/loops.java
+++ src/javaFullStack/loops.java
@@ -44,12 +44,20 @@ public class loops {
 //        }while (i < 10 || j < 10);
 
         // Break & Continue
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
-            if (i == 5) {
-                continue;
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.println(i);
+//            if (i == 5) {
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+
+        // Nested loops
+        for(int i = 1; i <= 10; i++) {
+            for(int j=1; j<=10;j++){
+                System.out.println("Value of i:"+i+" Value of j:"+j);
             }
-            System.out.println(i);
+            System.out.println("*********************************");
         }
     }
 
