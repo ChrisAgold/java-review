f022134541e8cd42a0b75f8c814a4faa47292cb6
Chris Agold <christophersagold@gmail.com>
2022-01-22 21:22:11 -0600

    refactored method

--- src/javaFullStack/methods.java
+++ src/javaFullStack/methods.java
@@ -1,27 +1,18 @@
 package javaFullStack;
-
 public class methods {
     public static void main(String[] args) {
         // CALL METHOD
-        loop1();
+        loop(2, 10);
         System.out.println("***********************************************");
-        loop2();
-    }
-
-    // METHOD
-    public static void loop1() {
-        int i = 1;
-        while (i <= 10) {
-            System.out.println(i);
-            i++;
-        }
+        loop(4, 8);
+        System.out.println("***********************************************");
+        loop(3, 9);
     }
     // METHOD
-    public static void loop2() {
-        int j = 20;
-        while (j <= 40) {
-            System.out.println(j);
-            j++;
+    public static void loop(int step, int finalValue) {
+        while (step <= finalValue) {
+            System.out.println(step);
+            step++;
         }
     }
 }
