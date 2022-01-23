35186924207e1014517d0f8fb2600c65e24e8d2d
Chris Agold <christophersagold@gmail.com>
2022-01-23 17:15:56 -0600

    area of rectangle method

--- src/javaFullStack/methods.java
+++ src/javaFullStack/methods.java
@@ -2,17 +2,25 @@ package javaFullStack;
 public class methods {
     public static void main(String[] args) {
         // CALL METHOD
-        loop(2, 10);
-        System.out.println("***********************************************");
-        loop(4, 8);
-        System.out.println("***********************************************");
-        loop(3, 9);
-    }
+//        loop(2, 10);
+//        System.out.println("***********************************************");
+//        loop(4, 8);
+//        System.out.println("***********************************************");
+//        loop(3, 9);
+//    }
     // METHOD
-    public static void loop(int step, int finalValue) {
-        while (step <= finalValue) {
-            System.out.println(step);
-            step++;
-        }
+//    public static void loop(int step, int finalValue) {
+//        while (step <= finalValue) {
+//            System.out.println(step);
+//            step++;
+//        }
+
+        // AREA OF RECTANGLE METHOD
+        System.out.println("Area of rectangle");
+        areaRectangle(5,3);
+    }
+    public static void areaRectangle(double length, double width) {
+        // AREA OF RECTANGLE METHOD
+        System.out.println(length * width);
     }
 }
