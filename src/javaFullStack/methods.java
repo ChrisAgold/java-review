1b30028b38b173b5c043b8e0614306a8e59b835e
Chris Agold <christophersagold@gmail.com>
2022-01-23 17:21:29 -0600

    added return statement

--- src/javaFullStack/methods.java
+++ src/javaFullStack/methods.java
@@ -1,4 +1,5 @@
 package javaFullStack;
+
 public class methods {
     public static void main(String[] args) {
         // CALL METHOD
@@ -8,19 +9,20 @@ public class methods {
 //        System.out.println("***********************************************");
 //        loop(3, 9);
 //    }
-    // METHOD
+        // METHOD
 //    public static void loop(int step, int finalValue) {
 //        while (step <= finalValue) {
 //            System.out.println(step);
 //            step++;
 //        }
 
-        // AREA OF RECTANGLE METHOD
-        System.out.println("Area of rectangle");
-        areaRectangle(5,3);
+        // AREA OF RECTANGLE METHOD CALLS
+        System.out.println("Area of rectangle " + areaRectangle(5,3));
+        //  double area = areaRectangle(5,3);
+        //  System.out.println(area);
     }
-    public static void areaRectangle(double length, double width) {
-        // AREA OF RECTANGLE METHOD
-        System.out.println(length * width);
+    // AREA OF RECTANGLE METHOD
+    public static double areaRectangle(double length, double width) {
+        return length * width;
     }
 }
