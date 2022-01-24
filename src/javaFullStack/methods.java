870debcde96b3584a5b28236dcdf930e314f1a7b
Chris Agold <christophersagold@gmail.com>
2022-01-24 17:58:59 -0600

    added method overloading example

--- src/javaFullStack/methods.java
+++ src/javaFullStack/methods.java
@@ -17,12 +17,32 @@ public class methods {
 //        }
 
         // AREA OF RECTANGLE METHOD CALLS
-        System.out.println("Area of rectangle " + areaRectangle(5,3));
+        // System.out.println("Area of rectangle " + areaRectangle(5,3));
         //  double area = areaRectangle(5,3);
         //  System.out.println(area);
+
+        // METHOD OVERLOADING CALL
+        System.out.println(       "Area of rectangle: " + area(14.25d, 10.65d));
+        System.out.println(       "Area of rectangle: " + area(5.0d));
+        System.out.println(       "Area of rectangle: " + area(7));
     }
+
     // AREA OF RECTANGLE METHOD
-    public static double areaRectangle(double length, double width) {
+//    public static double areaRectangle(double length, double width) {
+//        return length * width;
+//    }
+
+    // METHOD OVERLOADING AREA OF RECTANGLE
+    public static double area(double length, double width) {
         return length * width;
+    }
+    // METHOD OVERLOADING AREA OF SQUARE
+    public static double area(double side) {
+        return side * side;
+    }
+    // METHOD OVERLOADING AREA OF SQUARE
+    public static int area(int side) {
+        return side * side;
+
     }
 }
