170518590356e975311ebe26e150900c4cfbd99e
Chris Agold <christophersagold@gmail.com>
2022-01-25 20:11:44 -0600

    added method overloading example

--- src/javaFullStack/methods.java
+++ src/javaFullStack/methods.java
@@ -46,9 +46,10 @@ public class methods {
 //    }
 
         // METHOD OVERLOAD PART II
-        System.out.println("1+2=" + sum(1,2));
+        System.out.println("1+2=" + sum(1, 2));
         System.out.println("1+2.5=" + sum(1, 2.5F));
         System.out.println("1.2+2.5=" + sum(1.2, 2.5));
+        System.out.println("2+3+6=" + sum(2,3,6));
     }
 
     // METHOD OVERLOADING PART II
@@ -56,13 +57,20 @@ public class methods {
         System.out.println("Adding 2 int entities");
         return x + y;
     }
+
     public static float sum(int x, float y) {
         System.out.println("Adding 1 int & 1 float entity");
         return x + y;
     }
+
     public static double sum(double x, double y) {
         System.out.println("Adding 2 double entities");
         return x + y;
+    }
+
+    public static int sum(int x, int y, int z) {
+        System.out.println("Adding 3 int entities");
+        return x + y + z;
     }
 }
 
